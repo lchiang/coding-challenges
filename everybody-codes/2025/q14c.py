d08c850a57eeb895cfce394bf66bd9d6ffbbfa23
lchiang <lchiang@users.noreply.github.com>
2025-12-03 14:21:52 +0800

    everybodycode q14 done

--- everybody-codes/2025/q14c.py
+++ everybody-codes/2025/q14c.py
@@ -1,19 +1,15 @@
 
-f = open('in14c_test.txt')
+f = open('in14c.txt')
 patt = [list(x) for x in f.read().splitlines()]
 
-
-
-mapp = [['.' for _ in range(34)] for _ in range(34)]
+mapp = [['.' for _ in range(17)] for _ in range(17)]
 
 map_w = len(mapp[0])
 map_h = len(mapp)
 
-d = {}
-for y in range(map_h):
-    for x in range(map_w):
-        if mapp[y][x] == '#':
-            d[(x,y)] = '#'
+map_w, map_h = 17, 17
+
+d = {(x,y) for y in range(map_h) for x in range(map_w)}
 
 def print_dict(dd):
     for y in range(map_h):
@@ -38,11 +34,6 @@ def match_pattern(d):
                 break
     return match
 
-
-#13,14,15,16
-
-#print_dict(d)
-
 def num_active_neighbour_n_self(x,y):
     count = 0
     if (x, y) in d: count += 1
@@ -50,51 +41,58 @@ def num_active_neighbour_n_self(x,y):
     if (x-1, y+1) in d: count += 1
     if (x+1, y-1) in d: count += 1
     if (x+1, y+1) in d: count += 1
+
+    if x == map_w-1 and y == map_h-1:
+        if (x, y) in d: count += 1
+    elif x == map_w-1:
+        if (x, y-1) in d: count += 1
+        if (x, y+1) in d: count += 1
+    elif y == map_h-1:
+        if (x-1, y) in d: count += 1
+        if (x+1, y) in d: count += 1
     return count
 
-
-
-import time
-start = time.time()
-
-round = 0
-dn = d.copy()
+round = 1
 total_active = 0
+ta, seen = {}, {}
 
-import os
+TOTAL_ROUND = 1_000_000_000
 
-
-
-
-
-while round < 2000:
+while round < TOTAL_ROUND:
+    round += 1
+    dn = set()
     for y in range(map_h):
         for x in range(map_w):
             n = num_active_neighbour_n_self(x,y)
             if (n%2 == 0):
-                dn[(x,y)] = '#'
-            else:
-                if (x,y) in d:
-                    del dn[(x,y)]
-
-    #print(round)
-    
-    print('round', round)    
-    round += 1
-    if round % 1000 == 0:
-        print('round', round)    
-    total_active += len(dn)
-    
-    print_dict(d)
+                dn.add((x,y))
+    if match_pattern(dn):
+        total_active += len(dn)
+        print(f"Round {round}, active {len(dn)}, total active {total_active}")
+        ta[round] = len(dn)
+        
+        
+    state = frozenset(dn)
     d = dn.copy()
-    #print(total_active, map_active())
 
-
-
+    if state in seen:
+        num_loop = TOTAL_ROUND//(round - seen[state])
+        remaining_round = TOTAL_ROUND % (round - seen[state])
+        ta_before, ta_loop, ta_after = 0, 0, 0
+        for key, value in ta.items():
+            if 0 <= key < seen[state]:
+                ta_before += value
+                print(f"Key: {key}, Value: {value}")
+            if seen[state] <= key < round:
+                ta_loop += value
+                print(f"Key: {key}, Value: {value}")
+            if 0 <= key < remaining_round:
+                ta_after += value
+                print(f"Key: {key}, Value: {value}")
+            print('part 3', (ta_before+ta_loop*num_loop+ta_after)*4)
+        break
+    else:
+        seen[state] = round
 
 print('part 1, 2', total_active)
-print('===')
-
-end = time.time()
-print(end - start)
 
